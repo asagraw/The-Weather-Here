abb46892ebb0116e52fca7a105f9e41e6adc4998
asagraw <asagraw@github.com>
2019-06-12 06:33:28 +1000

    commit from git bash cmd

--- index.js
+++ index.js
@@ -1,30 +1,54 @@
 const express = require('express');
 const app = express();
 const Datastore = require('nedb');
-app.listen(3000, ()=> console.log('listening at port 3000'));
+const fetch = require('node-fetch');
+app.listen(3000, () => console.log('listening at port 3000'));
 app.use(express.static('public'));
-app.use(express.json({limit: '1mb'}));
+app.use(express.json({ limit: '1mb' }));
 var co_data = new Datastore('database.db');
 co_data.loadDatabase();
 app.get('/api', (request, response) => {
-    co_data.find({}, (err,data)=>{
-        if(err){
+    co_data.find({}, (err, data) => {
+        if (err) {
             response.end();
             return;
-        }else{
+        } else {
             response.json(data);
         }
     })
 });
-app.post('/api', (request, response)=>{
+app.post('/api', (request, response) => {
     const data = request.body;
     const timestamp = Date.now();
     data.timestamp = timestamp;
     co_data.insert(data);
     response.json({
-        status:'Success',
+        status: 'Success',
         latitude: data.lat,
         longitude: data.lon,
         timestamp: data.timestamp
     });
+})
+
+app.get('/weather/:latlon', async (request, response) => {
+    console.log(request.params);
+    const latlon = request.params.latlon.split(',');
+    console.log(latlon);
+    const lat = latlon[0];
+    const lon = latlon[1];
+    console.log(lat, lon);
+    // const api_url = 'https://api.darksky.net/forecast/3b3efcdbed54ae20f862a34b0fbd5b16/37.8267,-122.4233';
+    const api_url = `https://api.darksky.net/forecast/3b3efcdbed54ae20f862a34b0fbd5b16/${lat},${lon}/?units=si`;
+    console.log(api_url);
+    const api_resp = await fetch(api_url);
+    const api_json = await api_resp.json();
+
+    const aq_url = `https://api.openaq.org/v1/latest?coordinates=${lat},${lon}`;
+    const aq_response = await fetch(aq_url);
+    const aq_data = await aq_response.json();
+    const data = {
+        weather: api_json,
+        air_quality: aq_data
+    }
+    response.json(data);
 })
--- public/sketch.js
+++ public/sketch.js
@@ -2,43 +2,64 @@
 //     const canvas = createCanvas(160, 120);
 //     pixelDensity(1);
 //     background(0);
-    // noCanvas();
-    // const video = createCapture(VIDEO);
-    // video.size(320,240);
-    let lat, lon;
-    const button = document.getElementById('submit');
-    button.addEventListener('click', async event => {
-        // const mood = document.getElementById('mood').value;
-        // canvas.loadPixels();
-        // video.loadPixels();
-        // const image64 = canvas.elt.toDataURL();
-        // const image64 = video.canvas.toDataURL();
-        // const data = { lat, lon, mood, image64 };
-        const data = { lat, lon };
-        const options = {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(data)
-        };
-        const response = await fetch('/api', options);
-        const json = await response.json();
-        console.log(json);
-    });
+// noCanvas();
+// const video = createCapture(VIDEO);
+// video.size(320,240);
+let lat, lon;
+const button = document.getElementById('submit');
+button.addEventListener('click', async event => {
+    // const mood = document.getElementById('mood').value;
+    // canvas.loadPixels();
+    // video.loadPixels();
+    // const image64 = canvas.elt.toDataURL();
+    // const image64 = video.canvas.toDataURL();
+    // const data = { lat, lon, mood, image64 };
+    const data = { lat, lon };
+    const options = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+    };
+    const response = await fetch('/api', options);
+    const json = await response.json();
+    console.log(json);
+});
 
-    if ('geolocation' in navigator) {
-        console.log('geolocation available');
-        navigator.geolocation.getCurrentPosition(position => {
+if ('geolocation' in navigator) {
+
+    console.log('geolocation available');
+    navigator.geolocation.getCurrentPosition(async position => {
+        try {
             lat = position.coords.latitude;
             lon = position.coords.longitude;
             console.log(lat, lon);
-            document.getElementById('latitude').textContent = lat;
-            document.getElementById('longitude').textContent = lon;
-        });
-    } else {
-        console.log('geolocation not available');
-    }
+            document.getElementById('latitude').textContent = lat.toFixed(2);
+            document.getElementById('longitude').textContent = lon.toFixed(2);
+            const api_url = `weather/${lat},${lon}`;
+            // const api_url = `/weather`;
+            const api_resp = await fetch(api_url);
+            const json = await api_resp.json();
+            weather = json.weather.currently;
+            air = json.air_quality.results[0].measurements[0];
+            document.getElementById('summary').textContent = weather.summary;
+            document.getElementById('temp').textContent = weather.temperature;
+            // document.getElementById('CurrentTemp').textContent = weather.apparentTemperature;
+            document.getElementById('aq_parameter').textContent = air.parameter;
+            document.getElementById('aq_value').textContent = air.value;
+            document.getElementById('aq_units').textContent = air.unit;
+            document.getElementById('aq_date').textContent = air.lastUpdated;
+            console.log(json);
+        } catch (error) {
+            console.error(error);
+            air = { value: -1 };
+            document.getElementById('aq_value').textContent = 'NO READING';
+        }
+    });
+} else {
+    console.log('geolocation not available');
+}
 // }
 // function keyPressed() {
 //     if (key == 'c') {
